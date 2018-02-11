465956e38bfa31c399ae47b4967f962fc3d824e4
droidvim <droidvim@example.com>
2018-02-12 07:39:04 +0900

    Utility.csを修正

--- Utility.cs
+++ Utility.cs
@@ -4,12 +4,14 @@ using System.Security.Cryptography;
 namespace ConsoleGame{
 
 	static public class Utility{
+		
+		//一様乱数（0〜1）
 		public static double GetRandom(){
 			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-			byte[] bs = new byte[sizeof(Int32)];
+			byte[] bs = new byte[sizeof(UInt32)];
 			rng.GetBytes(bs);
-			int iR = System.BitConverter.ToInt32(bs, 0);
-			return ((double)iR / Int32.MaxValue);
+			int iR = System.BitConverter.ToUInt32(bs, 0);
+			return ((double)iR / UInt32.MaxValue);
 		}
 	}
 
