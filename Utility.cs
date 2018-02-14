a8df9e5271e1c907c8e3beba1e245a0053867090
droidvim <droidvim@example.com>
2018-02-14 17:01:54 +0900

    指定範囲の乱数を取得するメソッドを追加

--- Utility.cs
+++ Utility.cs
@@ -2,17 +2,25 @@ using System;
 using System.Security.Cryptography;
 
 namespace ConsoleGame{
+		static public class Utility{
 
-	static public class Utility{
-		
-		//一様乱数（0〜1）
-		public static double GetRandom(){
-			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-			byte[] bs = new byte[sizeof(UInt32)];
-			rng.GetBytes(bs);
-			UInt32 iR = System.BitConverter.ToUInt32(bs, 0);
-			return ((double)iR / UInt32.MaxValue);
+				//乱数の取得
+				private static UInt32 GetRandom(){
+					RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+					byte[] bs = new byte[sizeof(UInt32)];
+					rng.GetBytes(bs);
+					return System.BitConverter.ToUInt32(bs, 0);
+				}
+				
+				//一様乱数（0〜1）を取得
+				public static double GetRandom01(){
+					return ((double)GetRandom() / UInt32.MaxValue);
+				}
+
+				//指定範囲の整数の一様乱数を取得
+				public static int GetSelectedRandom(int min, int max)
+				{
+					return (int)((GetRandom() % max) + min);
+				}
 		}
-	}
-
 }
